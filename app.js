b6afc1c49ceb8b3fe8369652a68270d171ddcf29
Rahat Choudhury <RChoudhury@rms.com>
2019-11-05 14:55:36 -0500

    Build own API

---
+++ app.js
@@ -0,0 +1,134 @@
+//jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+const Article = mongoose.model(
+  "Article",
+  articleSchema
+);
+
+///////Requests Targetting All Articles///////////////////////////
+
+app.route("/articles")
+.get(function(req, res){
+  Article.find({}, function(err, foundArticles){
+    if(!err){
+    res.send(foundArticles);
+  } else {
+    res.send(err);
+  }
+
+  });
+})
+
+.post(function(req, res){
+  const article = new Article ({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  article.save(function(err){
+    if(!err) {
+      res.send("Data sent successfully!!");
+    } else {
+      res.send(err);
+    }
+  });
+
+  })
+
+  .delete(function(req, res){
+    Article.deleteMany({}, function(err){
+      if(!err){
+        res.send("Success!");
+      } else {
+        res.send("Process failed no doubt");
+      }
+    });
+  });
+
+
+///////Requests Targetting A Specific Articles///////////////////////////
+
+  app.route("/articles/:articleTitle")
+  .get(function(req, res){
+    const articleTitle = req.params.articleTitle;
+
+    Article.findOne({title: articleTitle}, function(err, foundArticle){
+      if(foundArticle){
+      res.send(foundArticle);
+    } else {
+      res.send("No article matcing the title was found!");
+    }
+
+    });
+  })
+
+  .put(function(req, res){
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      function(err){
+        if(!err) {
+          res.send("Update Successful");
+        } else {
+          res.semd("Sorry, that failed!");
+        }
+      }
+    );
+  })
+
+  .patch(function(req, res){
+    Article.update(
+      {title: req.params.articleTitle},
+      {$set: req.body},
+      function(err){
+        if(!err){
+          res.send("Patch was success!");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+
+  .delete(function(req, res){
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function(err){
+        if(!err){
+          res.send("Deleted Successfully");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
